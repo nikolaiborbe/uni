0b3675560f915463aafa710dc1caf7d22b42b3b6
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2024-10-13 20:34:14 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+#include <iostream>
+#include <string>
+#include <sstream>
 
+void getString(std::string& str) {
+    std::cout << "what sentence" << std::endl;
+    getline(std::cin, str);
+}
+
+void reverseWords(std::string& str) {
+    str = 
+    for (int i = 0; str.size(); i++) {
+        std::cout << i << std::endl;
     }
-};
+}
+
+int main() {
+    std::string str;
+    getString(str);
+    reverseWords(str);
+    std::cout << str << std::endl;
+
+    return 0;
+
+}
+
