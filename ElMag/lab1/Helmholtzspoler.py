8d6bff4456183efe383cd067465247be90c8e79f
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2025-02-04 11:37:02 +0100

    update

--- ElMag/lab1/Helmholtzspoler.py
+++ ElMag/lab1/Helmholtzspoler.py
@@ -12,27 +12,25 @@ def Helmholtzspoler(x, a):
     return (N*mu_0*I0)/(2*R) * (1 + (x - a/2)**2/R**2)**(-3/2) + (N*mu_0*I0)/(2*R) * (1 + (x + a/2)**2/R**2)**(-3/2)
 
 def get_data(file_path):
-    data =np.loadtxt(file_path, delimiter=',', encoding="utf-8-sig",)
+    data =np.loadtxt(file_path, delimiter=',', encoding="utf-8-sig", skiprows=1)
     x =data[:, 0]-x0   #posisjon,sentrertrundtx0
     y =data[:, 1]      #m˚aledata
     return x, y
 
 def main():
     x1, y1 = get_data("data/helmholtzspoler/2R.csv")
-    x1, y1 = get_data("data/helmholtzspoler/R_2.csv")
-    x1, y1 = get_data("data/helmholtzspoler/R.csv")
+    x2, y2 = get_data("data/helmholtzspoler/R_2.csv")
+    x3, y3 = get_data("data/helmholtzspoler/R.csv")
 
     #BeregnB-feltet
     xb =np.linspace(x1[0],x1[-1], 100)  #fleredatapunkter
     Bb1 = Helmholtzspoler(xb, R*2)*1e4       #beregnetB-felt(Gauss)
-    Bb2 = Helmholtzspoler(xb, R)*1e4       #beregnetB-felt(Gauss)
-    Bb3 = Helmholtzspoler(xb, R/2)*1e4       #beregnetB-felt(Gauss)
 
     #Plotresultatene
     plt.plot(xb,Bb1, label='Beregnet 1')
-    plt.plot(xb,Bb2, label='Beregnet 2')
-    plt.plot(xb,Bb3, label='Beregnet 3')
-    plt.plot(x1,y1, '.',label='M˚aledata')
+    plt.plot(x1,y1, '.',label='Måledata')
+    plt.plot(x2,y2, '.',label='Måledata')
+    plt.plot(x3,y3, '.',label='Måledata')
     plt.xlabel('Avstandfra senterav spolen(m)')
     plt.ylabel('Magnetfelt(Gauss)')
     plt.legend()
