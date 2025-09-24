83a1345dabdc22557dc1add51f2c2e11c8530187
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2025-09-24 16:04:52 +0200

    add term. plotting kode

--- Term/Lab_1/main.py
+++ Term/Lab_1/main.py
@@ -1,3 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+data = np.loadtxt("SB_lampe.csv", delimiter=",", dtype=str)
+temp = [np.float32(i) for i in data[1:, -1]]
+straalespenning_s = [np.float32(i) for i in data[1:, 2]]
 
+plt.plot(temp, straalespenning_s, marker="o", markeredgecolor="none", markerfacecolor="red", linestyle="--", color="black")
+plt.xlabel("temperatur (T) $[K]$")
+plt.ylabel("strålespenning (U) $[V]$")
+plt.tight_layout(pad=1.2)
+
+plt.legend()
+plt.show()
