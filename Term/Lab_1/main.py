8244ec5940e9cf21b8e6d293d56e38b101e4af0e
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2025-10-04 15:30:23 +0200

    improve graph

--- Term/Lab_1/main.py
+++ Term/Lab_1/main.py
@@ -1,4 +1,3 @@
-from cProfile import label
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,19 +5,21 @@ data = np.loadtxt("SB_lampe.csv", delimiter=",", dtype=str)
 temp = [np.float32(i) for i in data[1:, -1]]
 straalespenning_s = [np.float32(i) for i in data[1:, 2]]
 
-k = 8.9e-9 
+k = 8.85e-9 
 sigma = 5.67e-8
 x_vals = np.arange(700, 2700, 100)
 y_vals = [k*sigma*x**4 for x in x_vals]
 
-error = 0.0006
+yerror = 0.0006
 
 plt.plot(temp, straalespenning_s, marker="o", markeredgecolor="none", markerfacecolor="red", linestyle="--", color="black")
-plt.errorbar(temp, straalespenning_s, yerr=error, fmt="none", ecolor="black", capsize=5, capthick=2)
-plt.plot(x_vals, y_vals, label="$k \sigma T^4 \\text{ hvor } k = 9\cdot 10^{-9}$")
-plt.xlabel("temperatur (T) $[K]$")
-plt.ylabel("strålespenning (U) $[V]$")
-plt.tight_layout(pad=1.2)
+plt.errorbar(temp, straalespenning_s, yerr=yerror, fmt="none", ecolor="black", capsize=5, capthick=2)
+plt.plot(x_vals, y_vals, label="$k \sigma T^4 \\text{ hvor } k = 8.85\cdot 10^{-9}$")
+plt.xlabel("temperatur (T) $[K]$", fontsize=18)
+plt.ylabel("strålespenning (U) $[V]$", fontsize=18)
+plt.xticks(fontsize=16)
+plt.yticks(fontsize=16)
+#plt.tight_layout(pad=1.1)
 
-plt.legend()
+plt.legend(fontsize=20, fancybox=True)
 plt.show()
