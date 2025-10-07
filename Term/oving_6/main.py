e31c4fefb157b7cb54833c81ae89fa421e0ddc90
Nikolai <nikolaiborbe@gmail.com>
2025-10-07 15:59:49 +0200

    term num oving

---
+++ Term/oving_6/main.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+temps = np.arange(86, 287, 40)
+a = 1.37
+b = 0.0387
+N = 100
+
+def p(V, T):
+    R = 8.3144
+    # tror kanskje jeg trenger antall mol n
+    return (R*T)/(V-b) - a/V**2
+
+def p_ideel(V):
+    R = 8.3144
+    T = 286
+    # tror kanskje jeg trenger antall mol n
+    return (R*T)/V
+
+volumes = np.linspace(0.06, 0.6,len(temps))
+
+pressures = []
+for T in temps:
+    pressures.append([p(v, T) for v in volumes])
+
+for i in range(len(pressures)):
+    plt.plot(volumes, pressures[i], label=f"Temperature: {temps[i]} [K]")
+plt.plot(volumes, [p_ideel(v) for v in volumes], label="Ideel gass i T = 286 K", color="black")
+plt.xlabel("Volume [V]")
+plt.ylabel("Pressure [p]")
+plt.legend()
+plt.show()
+
+    
