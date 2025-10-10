5b15f6cd80afc75a84b1c1ea566f62bb17d727ac
Nikolai <nikolaiborbe@gmail.com>
2025-10-10 10:01:39 +0200

    .

--- Term/oving_6/main.py
+++ Term/oving_6/main.py
@@ -3,12 +3,15 @@ import numpy as np
 
 # Oppgave a
 temps = np.arange(86, 287, 40)
-a = 1.37
-b = 0.0387
+R = 8.3144
 N = 100
 
+# N_2 egenskaper
+a = 1.37
+b = 0.0387
+C_p = 1.04
+
 def p(V, T):
-    R = 8.3144
     # tror kanskje jeg trenger antall mol n
     return (R*T)/(V-b) - a/V**2
 
@@ -24,14 +27,25 @@ pressures = []
 for T in temps:
     pressures.append([p(v, T) for v in volumes])
 
-for i in range(len(pressures)):
-    plt.plot(volumes, pressures[i], label=f"Temperatur: {temps[i]} [K]")
-plt.plot(volumes, [p_ideel(v) for v in volumes], label="Ideel gass ved T = 286 K", color="black")
-plt.xlabel("Volum $V$ [L]")
-plt.xscale("log")
-plt.ylabel("Trykk $p$ [bar]")
-plt.legend()
-plt.show()
+# for i in range(len(pressures)):
+#     plt.plot(volumes, pressures[i], label=f"Temperatur: {temps[i]} [K]")
+# plt.plot(volumes, [p_ideel(v) for v in volumes], label="Ideel gass ved T = 286 K", color="black")
+# plt.xlabel("Volum $V$ [L]")
+# plt.xscale("log")
+# plt.ylabel("Trykk $p$ [bar]")
+# plt.legend()
+# plt.show()
 
 # Oppgave b
+T = np.arange(1,100, 1)
+p = np.arange(1,100, 1)
+V_ideell = R*T/p
+V_gammel = V_ideell
+for i in range(1, 30, 1):
+    V_ny = b + R*T/(p+a/V_gammel**2)
+    V_gammel = V_ny
+print(V_ny)
+
+# def mu_jt(T, p):
+#         if (R*T/C_p)/(p + a/V**2 - 2*a*(V-b)/V**3) - V/C_p:
  
