a34b6344dabe7bdc99c9de5d9577747e45d0116f
Nikolai <nikolaiborbe@gmail.com>
2025-10-24 09:37:13 +0200

    .

--- Term/oving_6/main.py
+++ Term/oving_6/main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Oppgave a
 temps = np.arange(86, 287, 40)
-R = 8.3144
+R = 0.08314
 N = 100
 
 # N_2 egenskaper
@@ -12,40 +12,59 @@ b = 0.0387
 C_p = 1.04
 
 def p(V, T):
-    # tror kanskje jeg trenger antall mol n
     return (R*T)/(V-b) - a/V**2
 
 def p_ideel(V):
-    R = 8.3144
     T = 286
-    # tror kanskje jeg trenger antall mol n
     return (R*T)/V
 
-volumes = np.linspace(0.06, 0.6,len(temps))
+volumes = np.linspace(0.06, 0.6,100)
 
 pressures = []
 for T in temps:
     pressures.append([p(v, T) for v in volumes])
 
-# for i in range(len(pressures)):
-#     plt.plot(volumes, pressures[i], label=f"Temperatur: {temps[i]} [K]")
-# plt.plot(volumes, [p_ideel(v) for v in volumes], label="Ideel gass ved T = 286 K", color="black")
-# plt.xlabel("Volum $V$ [L]")
-# plt.xscale("log")
-# plt.ylabel("Trykk $p$ [bar]")
-# plt.legend()
-# plt.show()
+for i in range(len(pressures)):
+    plt.plot(volumes, pressures[i], label=f"Temperatur: {temps[i]} [K]")
+plt.plot(volumes, [p_ideel(v) for v in volumes], label="Ideel gass ved T = 286 K", color="black")
+plt.xlabel("Volum $V$ [L]")
+plt.xscale("log")
+plt.ylabel("Trykk $p$ [bar]")
+plt.legend()
+plt.show()
 
 # Oppgave b
-T = np.arange(1,100, 1)
-p = np.arange(1,100, 1)
-V_ideell = R*T/p
-V_gammel = V_ideell
-for i in range(1, 30, 1):
-    V_ny = b + R*T/(p+a/V_gammel**2)
-    V_gammel = V_ny
-print(V_ny)
+T0 = 2*a / (R*b)
+T = np.linspace(T0/9, T0, 40)
+p = np.linspace(10e5, 310e5, 40)
+R = 8.314
+a = 0.137
+b = 3.87E-5
+C_p = 7/2 * R
+T_inv = np.linspace(T0 / 9, T0, 200)
+p_inv = (a / b**2) * (-3 * T_inv /  T0 + 4*np.sqrt(T_inv / T0)-1)
 
-# def mu_jt(T, p):
-#         if (R*T/C_p)/(p + a/V**2 - 2*a*(V-b)/V**3) - V/C_p:
- 
+TT, pp = np.meshgrid(T, p)
+
+V = R*TT/pp
+for _ in range(50):
+    V = b + R*TT/(pp + a/(V**2))
+
+def mu_jt(TT, pp, V):
+    denom = pp + a/(V**2) - 2*a*(V - b)/(V**3)
+    return (R*TT/C_p)/denom - V/C_p
+
+with np.errstate(divide='ignore', invalid='ignore'):
+    mu = mu_jt(TT, pp, V)
+    mu[~np.isfinite(mu)] = np.nan
+
+
+mu_bar = mu*1e5
+zmin, zmax = -0.2, 0.2
+
+fig, ax = plt.subplots(figsize=(0,6))
+im = ax.imshow(mu_bar, origin="lower", extent=[T.min(), T.max(), p.min()/1e5, p.max()/1e5], vmin=zmin, vmax=zmax)
+ax.set_ylabel("Trykk $p$ [bar]")
+ax.set_xlabel("Temperatur $T$ [K]")
+ax.plot(T_inv, p_inv * 1e-5, lw=2)
+plt.show()
