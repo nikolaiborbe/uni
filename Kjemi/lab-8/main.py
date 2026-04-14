78892c138efd6b7233a1f126556f5daf4ecd809b
Nikolai Grytvik Borbe <nikolaiborbe@gmail.com>
2026-04-14 15:48:09 +0200

    lab 8

--- Kjemi/lab-8/main.py
+++ Kjemi/lab-8/main.py
@@ -1,17 +1,23 @@
+import csv
 import matplotlib.pyplot as plt
-fig=plt.figure()
-################################### Experimental data
-#Volum=[15.7, ... 47.1] # ml
-#Volum=[v-Volum[0] for v in Volum]
-#Potential=[-131, ... 204] # mV
-Volum=[15.7, 39, 47.1] # ml
-Volum=[v-Volum[0] for v in Volum]
-Potential=[-131, 0, 204] # mV
-################################### Plot
-plt.plot(Volum,Potential,marker='+',linestyle='-')
-plt.grid()
-################################### save plot
-plt.ylabel('$E$ [mV]')
-plt.xlabel('$V$ [ml]')
-#plt.savefig('plot.pdf')
-plt.show()
+
+
+def plot_potential_per_volume(volume, potential):
+    v0 = volume[0]
+    vol = [v - v0 for v in volume]
+    plt.plot(vol, potential, marker="+", linestyle="-")
+    plt.grid()
+    plt.ylabel("$E$ [mV]")
+    plt.xlabel("$V$ [ml]")
+    plt.show()
+
+
+with open("data.csv") as f:
+    reader = csv.DictReader(f)
+    volume = []
+    potential = []
+    for row in reader:
+        volume.append(float(row["volume"]))
+        potential.append(float(row["potential"]))
+
+plot_potential_per_volume(volume, potential)
